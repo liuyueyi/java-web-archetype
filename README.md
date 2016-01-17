a766ae89857197b739c424bc846de43d4b187986
yihui <yihui@mogujie.com>
2016-01-17 10:24:46 +0800

    java web archetype

---
+++ README.md
@@ -0,0 +1,13 @@
+WebDemo
+===
+> a java project base on Spring 
+
+Struct
+===
+### 1. biz
+
+具体的业务代码工程
+
+### 2. web
+
+web层代码工程
---
+++ web/src/main/java/com/mushroom/hui/Index.java
@@ -0,0 +1,51 @@
+package com.mushroom.hui;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by yihui on 16/1/16.
+ */
+@Controller
+public class Index {
+    /**
+     * 返回字符串 index
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String index(HttpServletRequest request) {
+        return "index";
+    }
+
+
+    /**
+     * 返回网页，找名为 index.vm的模板返回
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/index")
+    public String index2(HttpServletRequest request) {
+        return "index";
+    }
+
+
+    @RequestMapping(value = "/view")
+    public ModelAndView getView(HttpServletRequest request) {
+        String id = request.getParameter("id");
+        if(StringUtils.isBlank(id)) {
+            id = "None";
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("view");
+        modelAndView.addObject("id", id);
+        return modelAndView;
+    }
+}
